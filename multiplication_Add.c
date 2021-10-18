fcc411662401279e26a5dad4035feb73cc75633e
Daniel41789 <89538726+Daniel41789@users.noreply.github.com>
2021-10-17 19:03:39 -0500

    fixed code

--- multiplication_Add.c
+++ multiplication_Add.c
@@ -6,6 +6,17 @@ int array[N] = {3, 12, 6, 8, 7};
 
 // This program multiplies the elements of the array by a number and then adds all its elements
 
+int mult(int a, int m)
+{
+    int i;
+    int producto = 0;
+    for(i = 0; i < m; i++)
+    {
+        producto = producto + a;
+    }
+    return producto;
+}
+
 int addArray(int *a, int lenght)
 {
 	int i;
@@ -19,27 +30,21 @@ int addArray(int *a, int lenght)
 	return add;
 }
 
-int multiplication(int *a, int lenght)
+int result(int *a, int lenght)
 {
 	int res;
 	int i;
 	int m = 2;
 	for (i=0; i<lenght; i++)
 	{ 
-		a[i]= array[i] * m;
+        a[i] = mult(array[i], m);
 	} 
-	
 	res = addArray(a, lenght);
 	return res;
-	
 }
 
 int main()
 {
-	int result = multiplication(array, N);
+	int res = result(array, N);
 	return 0;
 }
-
-
-
-
